5a05eefe8bf35bf744c66407c3875b0f077059c0
prajwal00017 <106654579+prajwal00017@users.noreply.github.com>
2022-12-06 15:45:25 +0530

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,9 +1,10 @@
-const cowsay = require("cowsay");
+var cowsay = require("cowsay");
+var Quote = require("inspirational-quotes")
+const quote = Quote.getRandomQuote()
 
-console.log(
-  cowsay.say({
-    text: "Piyush Mera Chuss",
-    e: "xx",
-    T:"x ",
-  })
-  );
+console.log(cowsay.say({
+  text: "quote",
+  e: "oO",
+  T: "U",
+}));
+
